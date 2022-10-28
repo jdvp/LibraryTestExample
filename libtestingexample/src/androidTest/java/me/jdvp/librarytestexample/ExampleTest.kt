0b449b76005086ee9d4e3e75c079f00c696f423d
JD Porterfield <jdrocks94@gmail.com>
2022-10-27 20:09:59 -0700

    No Ticket : Demonstrate MockK test slowdown in androidTest when mocking Application/Activity/etc.

---
+++ libtestingexample/src/androidTest/java/me/jdvp/librarytestexample/ExampleTest.kt
@@ -0,0 +1,42 @@
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.mockk
+import me.jdvp.librarytestexample.MainActivity
+import me.jdvp.librarytestexample.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleTest {
+    @Test
+    fun test_buttonClick_11() {
+        mockk<Fragment>()
+        launchActivity<MainActivity>()
+        Espresso.onView(ViewMatchers.withId(R.id.button_1)).perform(ViewActions.click())
+
+        verifyTextDisplayed("Hello, Button 1!")
+        verifyTextDisplayed("Thanks for clicking button 1!")
+    }
+
+    @Test
+    fun test_buttonClick_12() {
+        launchActivity<MainActivity>()
+        Espresso.onView(ViewMatchers.withId(R.id.button_2)).perform(ViewActions.click())
+
+        verifyTextDisplayed("Hello, Button 2!")
+        verifyTextDisplayed("Thanks for clicking button 2!")
+    }
+
+    private fun verifyTextDisplayed(text: String) {
+        Espresso.onView(ViewMatchers.withText(text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
